7d1ac79afa5845704f17eaced809a671c5c3b4ac
Nargis Parween <nargis.parween@unthinkable.co>
2019-08-05 20:18:27 +0530

    Adding node_modules in gitignore

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-var app = express();
+const app = express();
 
 const middleware = require('./middleware/middleware');
 const routes = require('./routes/index');
